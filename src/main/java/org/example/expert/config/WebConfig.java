7068defd3a81d98c0a5c8986ed985aba81238bde
escomputer <nujest307@naver.com>
2025-04-21 06:02:52 +0900

    Lv 4 : 4-1 interceptor
     세션기반
     userrole = admin인지 여부 확인 후
     admin이 아니라면 false 반환 - logging 안함
     admin 이라면 logging(uri, localdatetime)(true)

--- src/main/java/org/example/expert/config/WebConfig.java
+++ src/main/java/org/example/expert/config/WebConfig.java
@@ -1,8 +1,11 @@
 package org.example.expert.config;
 
 import lombok.RequiredArgsConstructor;
+import org.example.expert.interceptor.AdminAuthInterceptor;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -11,9 +14,16 @@ import java.util.List;
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
 
+
     // ArgumentResolver 등록
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new AuthUserArgumentResolver());
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry){
+        registry.addInterceptor(new AdminAuthInterceptor())
+                .addPathPatterns("/admin/**");
     }
 }
