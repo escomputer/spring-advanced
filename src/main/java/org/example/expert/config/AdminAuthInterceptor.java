ef06f1a5121157a19c0d10e627eaf02a36821cf8
escomputer <nujest307@naver.com>
2025-04-21 13:05:24 +0900

    Fix : Lv 4 : 4-1 Interceptor 수정
    filter에서 차단당하기 때문에 interceptor에서 굳이 차단 기능 안 필요함

--- src/main/java/org/example/expert/config/AdminAuthInterceptor.java
+++ src/main/java/org/example/expert/config/AdminAuthInterceptor.java
@@ -18,26 +18,12 @@ public class AdminAuthInterceptor implements HandlerInterceptor {
      *로그인 하고 세션에 JSESSIONID가 저장되어있으면  클라이언트 상태가 유지되고
      * 세션에서 userRole이 관리자 (admin,Admin,aDmin ~~,,,)일때
      * 관리자임을 인증할 수 있다.
+     * 모든
      *
      */
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object Handler) {
-        HttpSession session = request.getSession(false);
-
-        if (session == null) {
-            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-            return false;
-
-        }
-
-        String userRole = (String) session.getAttribute("user-role");
-
-        if (!"admin".equalsIgnoreCase(userRole)) {
-            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
-            return false;
-        }
-
         log.info("<관리자> url : {} , 시간 : {}", request.getRequestURI(), LocalDateTime.now());
         return true;
 
